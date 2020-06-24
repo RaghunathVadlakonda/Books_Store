a57fa77c7293cabbda18879b3f823250a0d639d7
Raghunath <Vadlakonda.raghu5@gmail.com>
2020-06-24 16:15:21 +0530

    Created Mongodb DB and connected to our application.

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+// require('dotenv').config()
 const express = require('express');
+const connectDB = require('./config/db');
+
+/**
+ * Create Express server.
+ */
 const app = express();
 
+
+/**
+ * Connect Database.
+ */
+connectDB();
+
+
+/**
+ * Express configuration.
+ */
 const PORT = process.env.PORT || 5000;
 
+
+/**
+ * Start Express server.
+ */
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`App is running at http://localhost:${PORT}`);
+    console.log("Press CTRL-C to stop\n");
 });
